84c54947869234888864eb3b463365d5b5881973
goldstargloww <goldstargloww@gmail.com>
2026-05-15 20:54:50 -0400

    shebang + uncomment logging

--- util.py
+++ util.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python3
 import sqlite3, re, os, logging, datetime, sys
 from alive_progress import alive_bar
 import custom_pytumblr as pytumblr
 from dotenv import load_dotenv
 
-# now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-# os.makedirs("logs/util", exist_ok=True)
-# logging.basicConfig(
-#     handlers=[logging.FileHandler(f"logs/util/{now}.log"), logging.StreamHandler(sys.stdout)],
-#     format="%(asctime)s [%(name)s] [%(levelname)s] %(message)s",
-# )
-# log = logging.getLogger("emojibot_util")
-# log.setLevel(logging.DEBUG)
+now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+os.makedirs("logs/util", exist_ok=True)
+logging.basicConfig(
+    handlers=[logging.FileHandler(f"logs/util/{now}.log"), logging.StreamHandler(sys.stdout)],
+    format="%(asctime)s [%(name)s] [%(levelname)s] %(message)s",
+)
+log = logging.getLogger("emojibot_util")
+log.setLevel(logging.DEBUG)
 
 load_dotenv()
 client = pytumblr.TumblrClient(

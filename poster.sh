39b8f2730eccddf4b4b34008508b91f80ed568af
goldstargloww <goldstargloww@gmail.com>
2026-06-13 01:22:08 -0400

    shell files begone

--- poster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/gold/Documents/Github/emoji-archive-bot
-source .venv/bin/activate
-python3 poster.py
-deactivate
--- scraper.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd "/media/gold/My Book/dualdocuments/coding/emoji-archive-bot"
-./.venv/bin/python scraper.py
